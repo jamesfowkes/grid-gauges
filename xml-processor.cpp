c29f1990b5a8d0c2b0ebff56e49ae82614827216
James Fowkes <jamesfowkes@gmail.com>
2018-11-26 09:59:29 +0000

    Add XML processor functionality and unit tests

--- xml-processor.cpp
+++ xml-processor.cpp
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include "xml-processor.h"
 
+#ifdef UNIT_TEST
+#include <iostream>
+#endif
+
 static char * find_string(char * haystack, char const * const needle, char * pEnd)
 {
     size_t needle_size = strlen(needle);
@@ -78,14 +82,21 @@ void XMLProcessor::process(char * xml, size_t length)
     copy_time(m_time, xml, p_XML_end);
     copy_total(m_total, xml, p_XML_end);
 
-    /*while (xml < p_inst_end)
-    {
-        xml = find_string(xml, "<FUEL_TYPE='", p_XML_end);
-        if (xml < p_inst_end)
-        {
+    char buffer[16];
 
+    while (xml)
+    {
+        xml = find_string_after(xml, "<FUEL ", p_XML_end);
+        if (xml)
+        {
+            copy_attr("TYPE", m_fuel_types[m_fuel_type_count], xml, p_XML_end);
+            copy_attr("VAL", buffer, xml, p_XML_end);
+            m_generation[m_fuel_type_count] = atoi(buffer);
+            copy_attr("PCT", buffer, xml, p_XML_end);
+            m_generation_pct[m_fuel_type_count] = (uint8_t)(atof(buffer) + 0.5f);
+            m_fuel_type_count++;
         }
-    }*/
+    }
 }
 
 char const * XMLProcessor::time()
@@ -96,6 +107,26 @@ char const * XMLProcessor::time()
 char const * XMLProcessor::total()
 {
     return m_total;
+}
+
+uint8_t XMLProcessor::fuel_type_count()
+{
+    return m_fuel_type_count;
+}
+
+char * XMLProcessor::get_fuel_type(uint8_t i)
+{
+    return m_fuel_types[i];
+}
+
+uint32_t XMLProcessor::get_fuel_generation(uint8_t i)
+{
+    return m_generation[i];
+}
+
+uint8_t XMLProcessor::get_fuel_percent(uint8_t i)
+{
+    return m_generation_pct[i];
 }
 
 #ifdef UNIT_TEST
@@ -109,11 +140,13 @@ class XMLProcessorTest : public CppUnit::TestFixture  {
     CPPUNIT_TEST_SUITE(XMLProcessorTest);
     CPPUNIT_TEST(test_time_processing);
     CPPUNIT_TEST(test_total_processing);
+    CPPUNIT_TEST(test_fuel_processing);
+
     CPPUNIT_TEST_SUITE_END();
 
     void test_time_processing()
     {
-        char xml[] = "<GENERATION_BY_FUEL_TYPE_TABLE><INST AT=\"2018-11-25 21:40:00\" TOTAL=\"34223\">";
+        char xml[] = "<GENERATION_BY_FUEL_TYPE_TABLE><INST AT=\"2018-11-25 21:40:00\" TOTAL=\"34223\"></INST>";
         XMLProcessor processor;
         processor.process(xml, strlen(xml));
         CPPUNIT_ASSERT_EQUAL(std::string("2018-11-25 21:40:00"), std::string(processor.time()));
@@ -121,10 +154,82 @@ class XMLProcessorTest : public CppUnit::TestFixture  {
 
     void test_total_processing()
     {
-        char xml[] = "<GENERATION_BY_FUEL_TYPE_TABLE><INST AT=\"2018-11-25 21:40:00\" TOTAL=\"34223\">";
+        char xml[] = "<GENERATION_BY_FUEL_TYPE_TABLE><INST AT=\"2018-11-25 21:40:00\" TOTAL=\"34223\"></INST>";
         XMLProcessor processor;
         processor.process(xml, strlen(xml));
         CPPUNIT_ASSERT_EQUAL(std::string("34223"), std::string(processor.total()));
+    }
+
+    void test_fuel_processing()
+    {
+        char xml[] = "<GENERATION_BY_FUEL_TYPE_TABLE><INST AT=\"2018-11-25 21:40:00\" TOTAL=\"34223\">"
+        "<FUEL TYPE=\"CCGT\" IC=\"N\" VAL=\"23105\" PCT=\"52.5\"/>"
+        "<FUEL TYPE=\"OCGT\" IC=\"N\" VAL=\"0\" PCT=\"0.0\"/>"
+        "<FUEL TYPE=\"OIL\" IC=\"N\" VAL=\"0\" PCT=\"0.0\"/>"
+        "<FUEL TYPE=\"COAL\" IC=\"N\" VAL=\"7356\" PCT=\"16.7\"/>"
+        "<FUEL TYPE=\"NUCLEAR\" IC=\"N\" VAL=\"6975\" PCT=\"15.8\"/>"
+        "<FUEL TYPE=\"WIND\" IC=\"N\" VAL=\"2338\" PCT=\"5.3\"/>"
+        "<FUEL TYPE=\"PS\" IC=\"N\" VAL=\"1055\" PCT=\"2.4\"/>"
+        "<FUEL TYPE=\"NPSHYD\" IC=\"N\" VAL=\"581\" PCT=\"1.3\"/>"
+        "<FUEL TYPE=\"OTHER\" IC=\"N\" VAL=\"72\" PCT=\"0.2\"/>"
+        "<FUEL TYPE=\"INTFR\" IC=\"Y\" VAL=\"0\" PCT=\"0.0\"/>"
+        "<FUEL TYPE=\"INTIRL\" IC=\"Y\" VAL=\"0\" PCT=\"0.0\"/>"
+        "<FUEL TYPE=\"INTNED\" IC=\"Y\" VAL=\"0\" PCT=\"0.0\"/>"
+        "<FUEL TYPE=\"INTEW\" IC=\"Y\" VAL=\"0\" PCT=\"0.0\"/>"
+        "<FUEL TYPE=\"BIOMASS\" IC=\"N\" VAL=\"2563\" PCT=\"5.8\"/>"
+        "<FUEL TYPE=\"INTNEM\" IC=\"Y\" VAL=\"0\" PCT=\"0.0\"/>"
+        "</INST>";
+
+        XMLProcessor processor;
+        processor.process(xml, strlen(xml));
+
+        CPPUNIT_ASSERT_EQUAL(15, (int)processor.fuel_type_count());
+
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(0)), std::string("CCGT"));
+        CPPUNIT_ASSERT_EQUAL(23105U, processor.get_fuel_generation(0));
+        CPPUNIT_ASSERT_EQUAL(53U, (unsigned int)processor.get_fuel_percent(0));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(1)), std::string("OCGT"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(1));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(1));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(2)), std::string("OIL"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(2));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(2));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(3)), std::string("COAL"));
+        CPPUNIT_ASSERT_EQUAL(7356U, processor.get_fuel_generation(3));
+        CPPUNIT_ASSERT_EQUAL(17U, (unsigned int)processor.get_fuel_percent(3));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(4)), std::string("NUCLEAR"));
+        CPPUNIT_ASSERT_EQUAL(6975U, processor.get_fuel_generation(4));
+        CPPUNIT_ASSERT_EQUAL(16U, (unsigned int)processor.get_fuel_percent(4));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(5)), std::string("WIND"));
+        CPPUNIT_ASSERT_EQUAL(2338U, processor.get_fuel_generation(5));
+        CPPUNIT_ASSERT_EQUAL(5U, (unsigned int)processor.get_fuel_percent(5));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(6)), std::string("PS"));
+        CPPUNIT_ASSERT_EQUAL(1055U, processor.get_fuel_generation(6));
+        CPPUNIT_ASSERT_EQUAL(2U, (unsigned int)processor.get_fuel_percent(6));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(7)), std::string("NPSHYD"));
+        CPPUNIT_ASSERT_EQUAL(581U, processor.get_fuel_generation(7));
+        CPPUNIT_ASSERT_EQUAL(1U, (unsigned int)processor.get_fuel_percent(7));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(8)), std::string("OTHER"));
+        CPPUNIT_ASSERT_EQUAL(72U, processor.get_fuel_generation(8));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(8));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(9)), std::string("INTFR"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(9));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(9));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(10)), std::string("INTIRL"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(10));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(10));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(11)), std::string("INTNED"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(11));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(11));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(12)), std::string("INTEW"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(12));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(12));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(13)), std::string("BIOMASS"));
+        CPPUNIT_ASSERT_EQUAL(2563U, processor.get_fuel_generation(13));
+        CPPUNIT_ASSERT_EQUAL(6U, (unsigned int)processor.get_fuel_percent(13));
+        CPPUNIT_ASSERT_EQUAL(std::string(processor.get_fuel_type(14)), std::string("INTNEM"));
+        CPPUNIT_ASSERT_EQUAL(0U, processor.get_fuel_generation(14));
+        CPPUNIT_ASSERT_EQUAL(0U, (unsigned int)processor.get_fuel_percent(14));
     }
 
 };
--- xml-processor.h
+++ xml-processor.h
@@ -1,18 +1,21 @@
 class XMLProcessor
 {
 public:
-	XMLProcessor();
+    XMLProcessor();
 
-	void process(char * xml, size_t length);
-	char const * time();
-	char const * total();
-	
+    void process(char * xml, size_t length);
+    char const * time();
+    char const * total();
+    uint8_t fuel_type_count();
+    char * get_fuel_type(uint8_t i);
+    uint32_t get_fuel_generation(uint8_t i);
+    uint8_t get_fuel_percent(uint8_t i);
 private:
-	void reset();
-	char m_time[32] = "";
-	char m_total[16] = "";
-	char * m_fuel_types[32][12];
-	uint32_t m_generation[32] = {0};
-	uint8_t m_generation_pct[32] = {0};
-	uint8_t m_fuel_type_count = 0;
+    void reset();
+    char m_time[32] = "";
+    char m_total[16] = "";
+    char m_fuel_types[32][12];
+    uint32_t m_generation[32] = {0};
+    uint8_t m_generation_pct[32] = {0};
+    uint8_t m_fuel_type_count = 0;
 };
